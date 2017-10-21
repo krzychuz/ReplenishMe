576fe1beb53f8987e6b3fa7d805a9d4e4b5e5afa
Krzysiek Zabawa <krzychuz@vp.pl>
2017-10-21 16:22:09 +0200

    Added JDBC driver support

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import db.Database;
 import init.DataLoader;
+import db.Test;
 
 public class Main {
 
     public static void main(String[] args) {
+        Test.createTestDbEntries();
         DataLoader.loadMaterialMaster();
         DataLoader.loadForecast();
         Database.generateMRPLists();
